38eb0068bb861f3b902fb0b3f17e19f634dbd132
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-13 00:19:12 +0530

    Update Guvi_print_hello_7.java

---
+++ Guvi_print_hello_7.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Guvi_print_hello_7
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int iter,i;
+		
+		iter=sc.nextInt();
+		
+		for(i=1;i<=iter;i++)
+			System.out.println("Hello");
+			
+	}
+}
