1cbd58738c95d6a7db212c57f8a76e6eacaa2dc5
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-12 22:56:19 +0530

    Update Guvi_even_odd_2.java

---
+++ Guvi_even_odd_2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Guvi_positive_negative_1
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int num;
+		
+		num=sc.nextInt();
+		
+		if(num%2==0)
+			System.out.println("");
+		else
+			System.out.println("Negative");
+		
+	}
+}
