035b79195580920640e36e287c5b8e17de7a02a1
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-15 23:41:14 +0530

    Update Guvi_multiples_20.java

---
+++ Guvi_multiples_20.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Guvi_multiples_20
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int num,i;
+		
+		num=sc.nextInt();
+		
+		for(i=1;i<=5;i++)
+		{
+			System.out.print(num*i+" ");
+		}
+		
+		System.out.println();
+	}
+}
