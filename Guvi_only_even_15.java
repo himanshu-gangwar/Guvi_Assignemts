ff7a6a44823af8da2a6064dad31ad4dd99504b9c
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-15 23:40:56 +0530

    Update Guvi_only_even_15.java

---
+++ Guvi_only_even_15.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Guvi_only_even_15
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int sr,er,i;
+		
+		sr=sc.nextInt();
+		er=sc.nextInt();
+
+		
+		for(i=sr;i<=er;i++)
+		{
+			if(i%2==0)
+				System.out.print(i+" ");
+			else
+				continue;
+		}
+		
+		System.out.println();
+	}
+}
