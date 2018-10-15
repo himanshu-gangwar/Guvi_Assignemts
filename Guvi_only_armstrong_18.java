c1bdd9d4fab532e4ca51b697da2ab80e78660bc4
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-15 23:41:07 +0530

    Update Guvi_only_armstrong_18.java

---
+++ Guvi_only_armstrong_18.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Guvi_only_armstrong_18
+{
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int sr,er,copy,rem,armno,i,num;
+		
+		sr=sc.nextInt();
+		er=sc.nextInt();
+
+		for(i=sr;i<=er;i++)
+		{
+			armno=0;
+			num=i;
+			
+			while(num!=0)
+			{
+				rem=num%10;
+				armno=armno+(rem*rem*rem);
+				num/=10;
+			
+			}
+			
+			if(i==armno)
+				System.out.println(i+" ");
+			else
+				continue;
+		}
+
+	}
+}
