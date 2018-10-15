f5adcd96efa7a1b28ed0871bfdeaab3eee051b64
Himanshu Gangwar <firsthimanshu@rediffmail.com>
2018-10-16 00:02:58 +0530

    Update Guvi_only_prime_16.py

---
+++ Guvi_only_prime_16.py
@@ -0,0 +1,16 @@
+def primerange(sr,er):
+    for i in range(sr,er,1):
+        pw=2**i
+        fer=pw-2
+        if(fer%i==0):
+            print(i," ")
+        else:
+            continue
+
+def main():
+    s=int(input("Enter starting range"))
+    e=int(input("Enter ending range"))
+    primerange(s,e)
+    
+if __name__=="__main__":
+    main()
